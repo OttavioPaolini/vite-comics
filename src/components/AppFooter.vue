791f24506e281c611905e5667324d98f00bf244d
Ottavio8 <paolini.ottavio8@gmail.com>
2022-11-08 22:02:23 +0100

    piccolo test con appfooter, si spacca tutto

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,26 @@
+<script>
+export default{
+    name: "AppFooter",
+    data(){
+        return
+    }
+}
+</script>
+
+<template>
+    <footer>
+        <div class="container">
+        
+        </div>
+
+    </footer>
+
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/general.scss" as *;
+@use "../styles/partials/mixin" as *;
+footer{
+    background-image: url(../assets/img/footer-bg.jpg);
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,18 @@
+<script>
+export default{
+    name: "AppMain",
+    data()
+}
+</script>
+
+<template>
+
+    <div>
+        <h1></h1>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
